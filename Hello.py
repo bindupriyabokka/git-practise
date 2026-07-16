18cbbed3667fd8d46e1cce4333141394dad56c04
Bindu Priya Bokka <bindupriyabokka@gmail.com>
2026-07-16 15:50:13 +0530

    Added Caculator.py in feature branch

--- Hello.py
+++ Hello.py
@@ -2,4 +2,4 @@ print("Hello")
 print("This is my first git practise")
 
 print("Learning Git from the  scratch")
-print(" Using the Visual Studio Code for the Git "practise")
+print(" Using the Visual Studio Code for the Git practise ")
